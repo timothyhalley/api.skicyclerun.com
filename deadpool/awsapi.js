abc19f831cf52d65a653efbfdb6f4ff55817f053
Timothy Halley <timothy.halley@gmail.com>
2018-12-18 17:05:00 -0800

    V2.1.001 - progress II

--- deadpool/awsapi.js
+++ deadpool/awsapi.js
@@ -1,4 +1,12 @@
 /*global require, module*/
+// Documentation:
+// AWS S3 API --> https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+const S3BUCKET = 'img.skicyclerun.com';
+const S3ALBUMS = 'albums';
+const S3PUBLIC = 'pub';
+const S3PRIVATE = 'private';
+const S3MAXKEYS = 100;
+
 var ApiBuilder = require('claudia-api-builder');
 var api = new ApiBuilder();
 var AWS = require('aws-sdk');
@@ -6,12 +14,6 @@ var S3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'us-west-2'
 });
-
-const S3BUCKET = 'img.skicyclerun.com';
-const FLDALBUMS = 'albums';
-const FLDPUBLIC = 'pub';
-const FLDPRIVATE = 'private';
-const MAXKEYS = 100;
 
 module.exports = api;
 
@@ -43,11 +45,13 @@ function s3KeyLists(params) {
 function getOneKey(data) {
 
   var allKeys = [];
-  var contents = data.Contents;
-  contents.forEach(function(content) {
-    allKeys.push(content.Key);
-  });
   var keyLength = data.KeyCount;
+  var contents = data.Contents;
+
+  for (let item of contents) {
+    allKeys.push(item.Key);
+  }
+
   var key = allKeys[Math.floor(Math.random() * keyLength)];
 
   return key;
@@ -57,33 +61,31 @@ var getOneS3Key = async (data) => {
 
   try {
 
-    //console.log('getOneS3Key data keys = ', data)
-    //data = await getOneKey(data).promise();
-    data = getOneKey(data);
+    return await getOneKey(data);
 
   } catch (err) {
 
-    console.log(err);
+    console.error(err)
     return err;
 
   }
-  console.log('URL key source is...  = ', data)
-  return data;
+
 };
 
 var eTagtoURL = async (cpResults) => {
 
   try {
 
-    //console.log('getOneS3Key data keys = ', data)
-    //data = await getOneKey(data).promise();
     var etag = cpResults.eTag;
     var url = null;
+
     if (etag) {
       url = 'https://' + cpResults.Bucket + '/' + cpResults.imgVal;
     } else {
-      url = 'https://img.skicyclerun.com/err/skicyclerun_error.jpg';
+      url = 'https://' + cpResults.Bucket + '/err/skicyclerun_error.jpg';
     }
+
+    return url;
 
   } catch (err) {
 
@@ -91,7 +93,7 @@ var eTagtoURL = async (cpResults) => {
     return err;
 
   }
-  return url;
+
 };
 
 function getETag(cpData) {
@@ -174,79 +176,147 @@ function copyRandomImage(params) {
   })
 };
 
+async function getKey(params) {
+
+  try {
+
+    let allKeys = [];
+
+    let data = await S3.listObjectsV2(params).promise();
+
+    for (let content of data.Contents) {
+      allKeys.push(content.Key);
+    };
+
+    return allKeys[Math.floor(Math.random() * data.KeyCount)]; // URI path to image source
+
+  } catch (e) {
+
+    return 'err/skicyclerun_error.jpg';
+
+  }
+}
+
+async function copyKey(keyIn) {
+
+  try {
+
+    let newImgPath = keyIn.replace(S3ALBUMS, S3PUBLIC);
+    console.log('THIS IS THE NEW IMG PATH: ', newImgPath);
+
+    var cpParams = {
+      Bucket: S3BUCKET,
+      CopySource: S3BUCKET + '/' + keyIn,
+      Key: newImgPath
+    };
+
+    let data = await S3.copyObject(cpParams).promise();
+
+    if (data.ETag) {
+      return newImgPath;
+    } else {
+      return 'err/skicyclerun_error.jpg';
+    }
+
+  } catch (err) {
+
+    return 'err/skicyclerun_error.jpg';
+
+  }
+}
 // ******************************************************************
 // API Gateway routes
 // ******************************************************************
-api.get('/randomimg/{album}', function(request) {
+//api.get('/randomimg/{album}', async function(request) {
+api.get('/getImage/{album}/{image}', async function(request) {
 
   'use strict';
+  // length of urlKey and ext is good then pass otherwise default
+  let uriAlbum = request.pathParams.album;
+  let uriImage = request.pathParams.image;
 
-  var urlAlbum = request.pathParams.album;
+  if (!uriAlbum) {
+    uriAlbum = 'SkiCycleRun';
+  };
+
+  if (!uriImage) {
+    uriImage = '000000.jpg';
+  }
 
   var params = {
     Bucket: S3BUCKET,
-    Prefix: FLDALBUMS,
-    MaxKeys: MAXKEYS
+    Prefix: S3ALBUMS + '/' + uriAlbum,
+    MaxKeys: S3MAXKEYS
   };
 
-  //Test Promise with Function return -- works!
-  return S3.listObjectsV2(params).promise()
-    .then(getOneS3Key)
-    .then(cpKey => S3.copyObject({
-      Bucket: params.Bucket,
-      CopySource: params.Bucket + '/' + cpKey,
-      Key: keyVal
-    }).promise())
-    .then(cpData => eTagtoURL({
-      Bucket: S3BUCKET,
-      imgVal: keyVal,
-      eTag: cpData
-    }))
+  const result = await getKey(params)
+    .then(copyKey);
+
+  return 'https://' + S3BUCKET + '/' + result;
 
 }, {
   success: 301
 });
 
-api.get('/getImage/{urlFld}/{urlKey}', function(request) {
+// api.get('/getImageAAA/{urlAlb}/{urlImg}', async function(request) {
+//
+//   'use strict';
+//
+//   // TODO - set folder in album to ramdomize!
+//
+//   var params = {
+//     Bucket: S3BUCKET,
+//     Prefix: S3ALBUMS,
+//     MaxKeys: S3MAXKEYS
+//   };
+//
+//   // length of urlKey and ext is good then pass otherwise default
+//   var urlAlb = request.pathParams.urlAlb;
+//   var urlImg = request.pathParams.urlImg;
+//   var imgFmt = urlKey.match(/(\d+).jpg/gi);
+//
+//   const result = await getKey(params)
+//     .then(copyKey)
+//
+//   let newImageURL = 'https://' + params.Bucket + '/' + result;
+//   return newImageURL;
+//
+// }, {
+//   success: 301
+// });
 
-  'use strict';
-
-  var params = {
-    Bucket: 'img.skicyclerun.com',
-    Prefix: 'albums',
-    MaxKeys: 100
-  };
-
-  // length of urlKey and ext is good then pass otherwise default
-  var urlFld = request.pathParams.urlFld;
-  var urlKey = request.pathParams.urlKey;
-  var urlImage = urlKey.match(/(\d+).jpg/gi);
-  var keyVal = urlFld + '/' + urlImage
-  if (urlImage) {
-    //Test Promise with Function return -- works!
-    return S3.listObjectsV2({
-        Bucket: S3BUCKET,
-        Prefix: FLDALBUMS,
-        MaxKeys: MAXKEYS
-      }).promise()
-      .then(getOneS3Key)
-      .then(cpKey => S3.copyObject({
-        Bucket: S3BUCKET,
-        CopySource: S3BUCKET + '/' + cpKey,
-        Key: keyVal
-      }).promise())
-      .then(cpData => eTagtoURL({
-        Bucket: S3BUCKET,
-        imgVal: keyVal,
-        eTag: cpData
-      }))
-  } else {
-    return urlImage = 'err/skicyclerun_error.jpg';
-  };
-
-}, {
-  success: 301
-});
+// api.get('/getImageX/{urlAlb}/{urlImg}', function(request) {
+//
+//   'use strict';
+//
+//   var params = {
+//     Bucket: S3BUCKET,
+//     Prefix: S3ALBUMS,
+//     S3MAXKEYS: S3MAXKEYS
+//   };
+//
+//   // length of urlKey and ext is good then pass otherwise default
+//   var urlAlb = request.pathParams.urlAlb;
+//   var urlImg = request.pathParams.urlImg;
+//   var imgFmt = urlKey.match(/(\d+).jpg/gi);
+//
+//   const result = await getKey(params)
+//   return S3.listObjectsV2(params).promise()
+//     .then(getKey)
+//     .then(cpKey => S3.copyObject({
+//       Bucket: S3BUCKET,
+//       CopySource: S3BUCKET + '/' + cpKey,
+//       Key: keyVal
+//     }).promise())
+//     .then(cpData => eTagtoURL({
+//       Bucket: S3BUCKET,
+//       imgVal: keyVal,
+//       eTag: cpData
+//     }))
+//
+// }, {
+//   success: 301
+// });
 
 api.get('/getImageV2/{urlAlbumName}/{urlImgRndNo}', function(request) {
 
@@ -259,8 +329,8 @@ api.get('/getImageV2/{urlAlbumName}/{urlImgRndNo}', function(request) {
   if (urlImage_is_Number) {
     var params = {
       Bucket: S3BUCKET,
-      Prefix: FLDALBUMS,
-      MaxKeys: MAXKEYS
+      Prefix: S3ALBUMS,
+      S3MAXKEYS: S3MAXKEYS
     };
     //Test Promise with Function return -- works!
     return S3.listObjectsV2(params).promise()
--- deadpool/test.js
+++ deadpool/test.js
@@ -1,3 +1,9 @@
+const S3BUCKET = 'img.skicyclerun.com';
+const S3ALBUMS = 'albums';
+const S3PUBLIC = 'pub';
+const S3PRIVATE = 'private';
+const S3MAXKEYS = 100;
+
 let AWS = require('aws-sdk');
 let lambda = new AWS.Lambda({
   region: 'us-west-2'
@@ -13,58 +19,7 @@ let data = null;
 // ***********************************************************************
 function getETag(cpData) {
 
-  var etag = cpData.ETag;
-  return etag;
-
-};
-
-var getS3ImageKeys = async (params) => {
-
-  try {
-
-    data = await S3.listObjectsV2(params).promise();
-
-  } catch (err) {
-
-    console.log(err);
-    return err;
-
-  }
-  //console.log('result of getS3ImageKeys  = ', data)
-  return data;
-};
-
-function getOneKey(data) {
-
-  var allKeys = [];
-  var contents = data.Contents;
-  var keyLength = data.KeyCount;
-
-  for (let content of contents) {
-  //contents.forEach(function(content) {
-    allKeys.push(content.Key);
-  };
-
-  var key = allKeys[Math.floor(Math.random() * keyLength)];
-
-  return key;
-}
-
-var getOneS3Key = async (data) => {
-
-  try {
-
-    let key = await getOneKey(data);
-
-    console.log('result of getOneS3Key  = ', key)
-    return key;
-
-  } catch (err) {
-
-    console.log(err);
-    return null;
-
-  }
+  return cpData.ETag;
 
 };
 
@@ -75,7 +30,7 @@ var copyS3Image = async (cpKey) => {
     var cpParams = {
       Bucket: S3BUCKET,
       CopySource: S3BUCKET + '/' + cpKey,
-      Key: cpKey.replace(subFolder, 'pub')
+      Key: cpKey.replace(S3ALBUMS, S3PUBLIC)
     };
 
     var data = await S3.copyObject(cpParams).promise();
@@ -90,31 +45,76 @@ var copyS3Image = async (cpKey) => {
   return data;
 };
 
-async function start(params){
+async function copyKey(keyIn) {
+
+  try {
+
+    var cpParams = {
+      Bucket: S3BUCKET,
+      CopySource: S3BUCKET + '/' + keyIn,
+      Key: keyIn.replace(S3ALBUMS, S3PUBLIC)
+    };
+
+    let data = await S3.copyObject(cpParams).promise();
+
+    if (data.ETag) {
+      return keyIn.replace(S3ALBUMS, S3PUBLIC)
+    } else {
+      return 'err/skicyclerun_error.jpg'
+    }
+
+    return data;
+
+  } catch (err) {
+
+    return err
+
+  }
+}
+
+async function getKey(params) {
+
+  try {
+
+    let allKeys = [];
+
+    let data = await S3.listObjectsV2(params).promise();
+
+    for (let content of data.Contents) {
+      allKeys.push(content.Key);
+    };
+
+    return allKeys[Math.floor(Math.random() * data.KeyCount)]; // URI path to image source
+
+  } catch (e) {
+
+    return 'err/skicyclerun_error.jpg';
+
+  }
+}
+
+async function start(params) {
+
     try {
 
-        const result = await getS3ImageKeys(params)
-          .then(getOneS3Key)
-          .then(copyS3Image);
-        var etag = getETag(result);
-        console.log('AWS S3 key Value = : ', result)
+        const result = await getKey(params)
+          .then(copyKey)
+
+        console.log('AWS S3 new URL Value = : ', result)
         return result;
 
     }
-    catch(error){
-        // Handle error
+
+    catch(err){
+        console.error('START ERROR: ', err)
     }
 }
 
 // ---------------------------------------------------
-const S3BUCKET = 'img.skicyclerun.com';
-const subFolder = 'albums';
-const pubFolder = 'pub';
-
 var params = {
   Bucket: S3BUCKET,
-  Prefix: subFolder,
-  MaxKeys: 55
+  Prefix: S3ALBUMS,
+  MaxKeys: S3MAXKEYS
 };
 
 (async () => {
